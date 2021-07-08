0a8082f6e8f065ffa3399d5e8bc8afddd3b35a82
Ken Key <ksquaredkey@users.noreply.github.com>
2021-07-08 11:17:46 -1000

    chore: Bump HomeBrew formula for 1.9.0

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/ksquaredkey/kontemplate"
-  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.8.1/kontemplate-1.8.1-656aa15-darwin-amd64.tar.gz"
-  sha256 "026d822f9b3df3bdba1db290e920fbc21c4ba9d74722c499069308290feb171e"
-  version "kontemplate-1.8.1-656aa15"
+  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.9.0/kontemplate-1.9.0-b5083f6-darwin-amd64.tar.gz"
+  sha256 "a98b31de486ebe9b8263f18290f5327942e22f1cf77fae583175220db91af8d9"
+  version "kontemplate-1.9.0-b5083f6"
 
   def install
     bin.install "kontemplate"
