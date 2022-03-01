eb3737122aaf8b31dd0ee24be05dc8f21c21e080
Ken Key <ksquaredkey@users.noreply.github.com>
2022-03-01 12:51:09 -1000

    Make release v1.10.1 fixing typo in Server Side Apply

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/ksquaredkey/kontemplate"
-  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.10.0/kontemplate-1.10.0-4503a01-darwin-amd64.tar.gz"
-  sha256 "97b229272780b3001ab797b829118cfa38d978e6f851e886a0241fbbd7685824"
-  version "kontemplate-1.10.0-4503a01"
+  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.10.0/kontemplate-1.10.1-f8d086a-darwin-amd64.tar.gz"
+  sha256 "4a52d37745355091cebea63e9b02d2242112c62b148087f4c646f52ad1921e6d"
+  version "kontemplate-1.10.1-f8d086a"
 
   def install
     bin.install "kontemplate"
