5c888dc85c67831f0f56bc549a7c4a22b161efdf
Ken Key <ksquaredkey@users.noreply.github.com>
2022-03-01 12:37:43 -1000

    Make release v1.10.0 for adding Server-Side Apply and cleaning up dry-run code

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/ksquaredkey/kontemplate"
-  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.9.0/kontemplate-1.9.0-b5083f6-darwin-amd64.tar.gz"
-  sha256 "a98b31de486ebe9b8263f18290f5327942e22f1cf77fae583175220db91af8d9"
-  version "kontemplate-1.9.0-b5083f6"
+  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.10.0/kontemplate-1.10.0-4503a01-darwin-amd64.tar.gz"
+  sha256 "97b229272780b3001ab797b829118cfa38d978e6f851e886a0241fbbd7685824"
+  version "kontemplate-1.10.0-4503a01"
 
   def install
     bin.install "kontemplate"
