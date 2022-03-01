1b6357896ba15efd33bdb62104d199ff827153e8
Ken Key <ksquaredkey@users.noreply.github.com>
2022-03-01 13:03:22 -1000

    Update to add '--ssa' flag for Server-Side Applies

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/ksquaredkey/kontemplate"
-  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.9.0/kontemplate-1.9.0-b5083f6-darwin-amd64.tar.gz"
-  sha256 "a98b31de486ebe9b8263f18290f5327942e22f1cf77fae583175220db91af8d9"
-  version "kontemplate-1.9.0-b5083f6"
+  url "https://github.com/ksquaredkey/kontemplate/releases/download/v1.10.0/kontemplate-1.10.1-f8d086a-darwin-amd64.tar.gz"
+  sha256 "4a52d37745355091cebea63e9b02d2242112c62b148087f4c646f52ad1921e6d"
+  version "kontemplate-1.10.1-f8d086a"
 
   def install
     bin.install "kontemplate"
